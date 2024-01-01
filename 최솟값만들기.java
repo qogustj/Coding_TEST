2188a841eec25c40fe137fd57bb37a9897116108
Bae Hyeonseo <haulqogustj@naver.com>
2024-01-01 20:17:00 +0900

    1.1 jadencase 문자열 만들기

---
+++ 최솟값만들기.java
@@ -0,0 +1,19 @@
+public class 최솟값만들기 {
+    public static void main(String[] args) throws Exception {
+
+        System.out.print(solution("for the last week"));
+
+    }
+
+
+
+    public static int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
