d5332c1294a095d5f9257229c21ca1424bf674ac
Bae Hyeonseo <haulqogustj@naver.com>
2024-02-27 23:17:14 +0900

    백준

--- 백준11052.java
+++ 백준11052.java
@@ -1,27 +1,27 @@
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.StringTokenizer;
-//
-//public class 백준11052 {
-//    static int n,m;
-//    static int[] arr;
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//        n=Integer.parseInt(st.nextToken());
-//        m=Integer.parseInt(st.nextToken());
-//        arr = new int[m];
-//        for(int i=1;i<n+1;i++){
-//            for(int j =1;j<n+1;j++ ){
-//                if(i!=j) {
-//                    System.out.print(i+" ");
-//                    System.out.println(j);
-//                }
-//            }
-//        }
-//    }
-//    static int[] solution(int[] arr){
-//        for
-//    }
-//}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 백준11052 {
+    static int n,m;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        for(int i=1;i<n+1;i++){
+            for(int j =1;j<n+1;j++ ){
+                if(i!=j) {
+                    System.out.print(i+" ");
+                    System.out.println(j);
+                }
+            }
+        }
+    }
+    static int[] solution(int[] arr){
+        for
+    }
+}
