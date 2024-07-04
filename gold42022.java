a749709781b98706d21d699676f0f129c07ffa84
qogustj <haulqogustj@naver.com>
2024-07-04 18:01:36 +0900

    7.4

---
+++ gold42022.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class gold42022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x,y,c;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+
+    }
+}
