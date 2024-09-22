c63c0684132f95d8a7507bdf516f4158b5bd56a8
배현서 <haulqogustj@naver.com>
2024-09-22 20:50:53 +0900

    9.22

--- start/simul_g5백준12919.java
+++ start/simul_g5백준12919.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class g5백준12919 {
+public class simul_g5백준12919 {
     static String S,T;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
