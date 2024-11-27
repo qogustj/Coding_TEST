f682a7d6daec291dc73dbe3aea78399c0fed701a
배현서 <haulqogustj@naver.com>
2024-11-27 14:25:02 +0900

    문제 요약: - 0부터 N까지의 정수 중 K개를 선택해서 합이 N이 되는 경우의 수를 구하는 문제 - 특징: 중복 허용, 순서가 다르면 다른 경우로 취급
    해결 방법:
    - DP를 사용한 풀이
    - dp[i][j] = i개의 숫자를 써서 합이 j가 되는 경우의 수
    - 점화식: dp[i][j] = sum(dp[i-1][j-l]) (l은 0부터 j까지)
    
    예를 들어 N=3, K=2인 경우:
    ```
    (0,3), (1,2), (2,1), (3,0)
    이런 식으로 K개의 숫자를 조합해서 N을 만드는 모든 경우를 구함
    ```
    
    이 문제의 핵심은 "단순 재귀/DFS로는 시간초과가 나므로 DP로 풀어야 한다"

---
+++ main/dp_g5.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.io.*;
+import java.util.*;
+
+public class dp_g5 {
+    static int n, k;
+    static int result = 0;
+    static final int MOD = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        long[][] dp = new long[k+1][n+1];
+
+        for(int i=0; i<=n; i++){
+            dp[1][i] = 1;
+        }
+
+        for(int i=2; i<=k; i++){
+            for(int j=0; j<=n; j++){
+                for(int l=0; l<=j; l++){
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-l]) % MOD;
+                }
+            }
+        }
+        System.out.print(dp[k][n]);
+    }
+
+
+}
